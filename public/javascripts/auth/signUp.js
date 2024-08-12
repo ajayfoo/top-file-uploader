ae4f152689e15f9028ab4f65d7e8a523bb006af2
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-08-12 20:51:27 +0530

    feat: validate password and confirm password for signup

--- public/javascripts/auth/signUp.js
+++ public/javascripts/auth/signUp.js
@@ -1,43 +1,74 @@
 const form = document.getElementById("sign-up-form");
-const allInputs = Array.from(form).filter((ele) => ele.tagName !== "BUTTON");
 
 const validateInput = (input) => {
-  const { showValidationMessage } = inputMap.get(input);
+  const { showValidationMessage, messages } = inputMap.get(input);
   if (!showValidationMessage) {
     input.classList.remove("invalid");
     return;
   }
   input.classList.add("invalid");
-  const { valueMissing, tooShort } = input.validity;
+  const { valueMissing, tooShort, patternMismatch, customError } =
+    input.validity;
   const messageEle = input.parentElement.querySelector(".error>.message");
   if (valueMissing) {
     messageEle.textContent = "Required";
   } else if (tooShort) {
     messageEle.textContent =
       "Must be at least " + input.minLength + " character(s) long";
+  } else if (patternMismatch) {
+    messageEle.textContent = messages.patternMismatch;
+  } else if (customError) {
+    messageEle.textContent = messages.customError;
   } else {
     messageEle.textContent = "Invalid input";
   }
 };
 
+const validateConfirmPassword = () => {
+  const passwordEle = document.getElementById("password");
+  const confirmPasswordEle = document.getElementById("confirm-password");
+  if (confirmPasswordEle.value !== passwordEle.value) {
+    confirmPasswordEle.setCustomValidity("Passwords must match");
+  } else {
+    confirmPasswordEle.setCustomValidity("");
+  }
+};
+
 const inputMap = new Map();
-allInputs.forEach((i) => {
-  let edited = false;
-  const showValidationMessageObj = { showValidationMessage: false };
-  inputMap.set(i, showValidationMessageObj);
-  i.addEventListener("input", () => {
-    edited = true;
-    if (i.checkValidity()) {
-      showValidationMessageObj.showValidationMessage = false;
-    }
-    validateInput(i);
+const populateInputMap = () => {
+  const usernameEle = document.getElementById("username");
+  const passwordEle = document.getElementById("password");
+  const confirmPasswordEle = document.getElementById("confirm-password");
+  const allInputs = [usernameEle, passwordEle, confirmPasswordEle];
+  allInputs.forEach((i) => {
+    let edited = false;
+    const showValidationMessageObj = { showValidationMessage: false };
+    inputMap.set(i, showValidationMessageObj);
+    i.addEventListener("input", () => {
+      edited = true;
+      if (i === confirmPasswordEle) {
+        console.log("validating confirm password");
+        validateConfirmPassword();
+      }
+      if (i.checkValidity()) {
+        showValidationMessageObj.showValidationMessage = false;
+      }
+      validateInput(i);
+    });
+    i.addEventListener("blur", () => {
+      if (!edited || i.checkValidity()) return;
+      showValidationMessageObj.showValidationMessage = true;
+      validateInput(i);
+    });
   });
-  i.addEventListener("blur", () => {
-    if (!edited || i.checkValidity()) return;
-    showValidationMessageObj.showValidationMessage = true;
-    validateInput(i);
-  });
-});
+  inputMap.get(passwordEle).messages = {
+    patternMismatch:
+      "Must contain at least one uppercase and lowercase letter, number and special symbol",
+  };
+  inputMap.get(confirmPasswordEle).messages = {
+    customError: "Passwords must match",
+  };
+};
 
 const validateAllInputs = () => {
   for (const [input, value] of inputMap.entries()) {
@@ -46,9 +77,12 @@ const validateAllInputs = () => {
   }
 };
 
+populateInputMap();
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (!form.checkValidity()) {
     validateAllInputs();
+  } else {
+    console.log("ready to submit");
   }
 });
