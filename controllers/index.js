362c493ba59e0fe8e6b0932db4af35088913474f
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-08-14 19:48:23 +0530

    feat: add markdown for add-menu and banner

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,15 @@
 import multer from "multer";
 import { saveFiles } from "../utils.js";
+import db from "../db.js";
 
-const renderIndex = (req, res) => {
-  res.render("index");
+const renderIndex = async (req, res) => {
+  const { username } = await db.user.findUnique({
+    where: {
+      id: req.session.passport.user.id,
+    },
+  });
+  console.log(username);
+  res.render("index", { username, folderName: "Root" });
 };
 
 const storage = multer.memoryStorage();
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,3 +3,28 @@ const addFileDialog = document.getElementById("add-files-dialog");
 addFilesButton.addEventListener("click", () => {
   addFileDialog.showModal();
 });
+
+const setupAddMenu = () => {
+  const menuHeader = document.getElementById("add-menu-header");
+  const itemsEle = document.querySelector("#add-menu>.items");
+  const firstItem = document.querySelector("#add-menu>.items>:first-child");
+  console.log(firstItem);
+  let open = false;
+  menuHeader.addEventListener("click", () => {
+    if (open) {
+      itemsEle.classList.remove("visible");
+    } else {
+      itemsEle.classList.add("visible");
+      firstItem.focus();
+    }
+    open = !open;
+  });
+};
+
+setupAddMenu();
+
+const addFolderButton = document.getElementById("add-folder-button");
+const addFolderDialog = document.getElementById("add-folder-dialog");
+addFolderButton.addEventListener("click", () => {
+  addFolderDialog.showModal();
+});
