b0bdc2db4384854248efdda26b4fc64ff220bf0b
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-08-20 23:03:38 +0530

    feat: show file size with units on file info page

--- utils.js
+++ utils.js
@@ -2,13 +2,43 @@ import { fileTypeFromBuffer } from "file-type";
 import mime from "mime/lite";
 import db from "./db.js";
 
+const getSizeUnits = (e) => {
+  switch (e) {
+    case 1:
+      return "KB";
+    case 2:
+      return "MB";
+    case 3:
+      return "GB";
+    case 4:
+      return "TB";
+    default:
+      return "??";
+  }
+};
+
+const getDisplayFileSize = (sizeInBytes) => {
+  for (let e = 4; e >= 1; --e) {
+    const formattedFileSize = sizeInBytes / Math.pow(10, 3 * e);
+    if (formattedFileSize >= 1)
+      return formattedFileSize.toFixed(2) + " " + getSizeUnits(e);
+  }
+  return sizeInBytes + " Bytes";
+};
+
 const getFormattedFileInfo = async (file) => {
   const originalname = file.originalname;
+  const formattedFileInfo = {
+    name: originalname,
+    mimeType: mime.getType(originalname),
+    size: file.size,
+    displaySize: getDisplayFileSize(file.size),
+  };
   const result = await fileTypeFromBuffer(file.buffer);
-  if (!result) {
-    return { name: originalname, mimeType: mime.getType(originalname) };
+  if (result) {
+    formattedFileInfo.mimeType = result.mime;
   }
-  return { name: originalname, mimeType: result.mime };
+  return formattedFileInfo;
 };
 
 const saveFiles = async (files, ownerId, parentId) => {
