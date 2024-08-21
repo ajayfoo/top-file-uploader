df0e1e9a6ba9616404e64d399db9dc589f102081
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-08-21 12:22:41 +0530

    feat: show failure message when file deletion failed

--- public/javascripts/fileInfo.js
+++ public/javascripts/fileInfo.js
@@ -6,16 +6,26 @@ deleteFileBtn.addEventListener("click", () => {
   confirmDeleteFileDialog.showModal();
 });
 
+const showFailedToDeleteFileDialog = () => {
+  const dialog = document.getElementById("failed-to-delete-file-dialog");
+  dialog.showModal();
+};
+
 confirmDeleteFileDialog.addEventListener("submit", async (e) => {
   if (document.activeElement.hasAttribute("formnovalidate")) return;
   e.preventDefault();
-  const response = await fetch(location.href, {
-    method: "DELETE",
-  });
-  if (response.ok) {
-    console.log("successfully deleted");
-    location.assign(location.origin);
-  } else {
-    console.log("failed to delete");
+  try {
+    const response = await fetch(location.href, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      location.assign(location.origin);
+    } else {
+      showFailedToDeleteFileDialog();
+    }
+  } catch {
+    showFailedToDeleteFileDialog();
+  } finally {
+    confirmDeleteFileDialog.close();
   }
 });
